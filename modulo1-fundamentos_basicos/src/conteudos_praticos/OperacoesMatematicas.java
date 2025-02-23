edebc2f8181691587b2939884bc237d2efc1f810
Marx dos Santos <marx.softwaredeveloper@gmail.com>
2025-02-23 17:55:16 -0300

    Commit Code: Conteúdo Fundamentos Básicos

---
+++ modulo1-fundamentos_basicos/src/conteudos_praticos/OperacoesMatematicas.java
@@ -0,0 +1,76 @@
+/* OPERAÇÕES MATEMÁTICAS EM JAVA:
+ * A classe Math do Java fornece métodos para realizar operações matemáticas básicas e avançadas, 
+ * como cálculos de potência, arredondamento, geração de números aleatórios, trigonometria e mais.
+ */
+
+package conteudos_praticos;
+
+public class OperacoesMatematicas {
+
+	public static void main(String[] args) {
+		// Exemplos Práticos
+
+		System.out.println("1. Obtendo o maior e o menor valor entre dois números com os métodso Math.max() e Math.min():");
+		int a = 10, b = 20;
+		System.out.println("Maior número: " + Math.max(a, b));
+		System.out.println("Menor número: " + Math.min(a, b));
+		System.out.println();
+
+		System.out.println("2. Obtendo a potenciação com o método Math.pow():");
+		double base = 3, expoente = 4;
+		double resultado = Math.pow(base, expoente);
+		System.out.println("O resultado de 3 elevado a 4 potência é: " + resultado);
+		System.out.println();
+
+		System.out.println("3. Obtendo a raiz quadrada com o método Math.sqrt():");
+		double numero = 49;
+		System.out.println("A raiz quadrada de 49 é: " + Math.sqrt(numero));
+		System.out.println();
+
+		System.out.println("4. Obtendo um valor absoluto com o método Math.abs():");
+		int numeroAbs = -10;
+		System.out.println("O valor absoluto de -10 é: " + Math.abs(numeroAbs));
+		System.out.println();
+
+		System.out.println("6. Arrendondando um valor com os métodos  Math.round(), Math.ceil(), Math.floor():");
+		double numeroArd = 7.6;
+		System.out.println("Arrendodando o valor 7.6: " + Math.round(numeroArd));
+		System.out.println("Arrendodando o valor 7.6 para cima: " + Math.ceil(numeroArd));
+		System.out.println("Arrendodando o valor 7.6 para baixo: " + Math.floor(numeroArd));
+		System.out.println();
+
+		System.out.println("6. Gerando números aleatórios com o método Math.random():");
+		double aleatorio = Math.random(); // Retorna um número entre 0.0 e 1.0
+		int aleatorioEntre1e10 = (int) (Math.random() * 10) + 1; // Convertendo um número aleatório para int
+		System.out.println("Número aleatório: " + aleatorio);
+		System.out.println("Número aleatório entre 1 e 10: " + aleatorioEntre1e10);
+		System.out.println();
+
+		System.out.println("7. Gerando o número PI e Euler com os métodos Math.PI e Math.E");
+		System.out.println("Valor de PI: " + Math.PI);
+		System.out.println("Valor de Euler: " + Math.E);
+		System.out.println();
+
+		System.out.println("8. Gerando o número Logaritmo Natural e Logaritmo Base 10 com os métodos Math.log() e Math.log10():");
+		double numeroLog = 100;
+		System.out.println("Logaritmo natural de 100: " + Math.log(numeroLog));
+		System.out.println("Logaritmo base 10 de 100: " + Math.log10(numeroLog));
+		System.out.println();
+
+		System.out.println("9. Gerando Funções Trigonométricas com os métodos Math.sin(), Math.cos(), Math.tan())");
+		double angulo = Math.toRadians(45); // Converte 45° para radianos
+		System.out.println("Seno de 45°: " + Math.sin(angulo));
+		System.out.println("Cosseno de 45°: " + Math.cos(angulo));
+		System.out.println("Tangente de 45°: " + Math.tan(angulo));
+		System.out.println();
+
+		System.out.println("10. Convertendo Graus para Radianos e vice-versa com os métodos Math.toRadians(),  Math.toDegrees():");
+		double graus = 180;
+		double radianos = Math.toRadians(graus);
+		System.out.println("180 graus em radianos: " + radianos);
+		System.out.println("π radianos em graus: " + Math.toDegrees(Math.PI));
+		System.out.println();
+
+	}
+
+}
---
+++ modulo1-fundamentos_basicos/src/conteudos_praticos/Variaveis.java
@@ -0,0 +1,106 @@
+package conteudos_praticos;
+
+/* VARIÁVEIS: 
+ * Uma variável é um espaço na memória do computador onde podemos armazenar 
+ * um valor que pode ser usado ou modificado durante a execução do programa. 
+ */
+
+/* Em Java, existem diferentes tipos de variáveis, por exemplo:
+ * String: armazena texto, como "Olá". Valores de string são cercados por aspas duplas.
+ * int: armazena números inteiros (números inteiros), sem decimais, como 123 ou -123.
+ * float: armazena números de ponto flutuante, com decimais, como 19,99 ou -19,99.
+ * char: armazena caracteres únicos, como 'a' ou 'B'. Os valores char são cercados por aspas simples.
+ * boolean: armazena valores com dois estados: verdadeiro ou falso.
+ */
+					
+/* Em Java, toda variável possui:
+ * Um tipo (que define o tipo de dado armazenado).
+ * Um nome (identificador).
+ * Um valor (o dado propriamente dito).
+ * Um escopo (a região do programa onde a variável é acessível). 
+ */
+
+/* As regras gerais para nomear variáveis ​​são:
+ * Os nomes podem conter letras, dígitos, sublinhados e cifrões
+ * Os nomes devem começar com uma letra
+ * Os nomes devem começar com uma letra minúscula e não podem conter espaços em branco
+ * Os nomes também podem começar com $ e _
+ * Os nomes diferenciam maiúsculas de minúsculas ("myVar" e "myvar" são variáveis ​​diferentes)
+ * Palavras reservadas (como palavras-chave Java, como intou boolean) não podem ser usadas como nomes
+ */
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		/* DECLARANDO VARIÁVEIS:
+		 
+		 Para criar uma variável, devemos especificar o tipo e atribuir um valor a ela: 
+		 Sintaxe: type variableName = value;
+		 
+		 - type é um dos tipos do Java
+		 - variableName é o nome da variável (como x ou name ). 
+		 - O sinal de igual é usado para atribuir valores à variável.
+		*/
+		
+		//EXEMPLOS DE DECLARAÇÃO DE VARIÁVEIS:
+		
+		// Criando uma variável para armazenar texto:
+		String nome = "Marx";
+		System.out.println("Meu nome é " + nome);
+		
+		// Criando uma variável para armazenar um número:
+		int idade = 29;
+		System.out.println("Eu tenho " + idade + " anos.");
+		
+		// Podemos criar uma variável sem atribuir um valor e atribuir o valor depois:
+		double altura;
+		altura = 1.86;
+		System.out.println("Eu tinha " + altura + " de altura.");
+		
+		// Se atribuir um novo valor a uma variável existente, ele substituirá o valor anterior:
+		altura = 1.96;
+		System.out.println("Agora tenho " + altura);
+		
+		// Podemos utilizar o caractere + para adicionar uma variável a outra variável:
+		String primeiroNome = "Marx ";
+		String segundoNome = "Santos";
+		String nomeCompleto = primeiroNome + segundoNome;
+		System.out.println("Meu nome completo é " + nomeCompleto);
+		
+		// Podemos declarar mais de uma variável do mesmo tipo utilizando virgulas para separa-las:
+		int x = 5, y = 32, z = 22;
+		System.out.println(x + y + z);
+		
+		// Podemos atribuir o mesmo valor para várias variáveis em uma linha:
+		int a, b, c;
+		a = b = c = 43;
+		System.out.println(a + b + c);
+		
+		
+		/* Podemos criar variáveis que quando atribuimos uma valor a ela, não podemos mais alterar.
+		 * Para fazer isso, devemos utilizar a palavra-chave "final", isso declarará a variável 
+		 * como final ou constante, o que significa imutável.
+		 */
+		final String saudacaoFixa = "Olá, bom dia!";
+		System.out.println(saudacaoFixa);
+		
+		System.out.println();
+		
+		/* EXERCÍCIO DE FIXAÇÃO
+		 * Programa para calcular a área de um retângulo (multiplicando o comprimento pela largura):
+		 */
+				
+		// Declarando as variáveis
+		int comprimento = 10;
+		int largura = 20;
+		int area;
+		
+		// Calculando a área do retangulo:
+		area = comprimento * largura;
+		
+		// Imprimindo as variáveis:
+		System.out.println("O comprimento do retangulo é: " + comprimento);
+		System.out.println("A largura do retangulo é: " + largura);
+		System.out.println("A área do retando é: " + area);	
+	}
+}
